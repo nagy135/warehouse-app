0f15736717e7d7e1f5e415cc3ec1380abb27cd85
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-08-04 13:19:34 +0200

    feat: typescript

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import "./styles.css";
+
+export default function App() {
+	return (
+		<View className="p-3 items- justify-center flex-col-reverse ">
+			<View className="border-black p-3">
+				<Text>Open up App.js to start working on your app!! 1111</Text>
+				<Text>lol</Text>
+			</View>
+			<View className="border-black p-3">
+				<Text>Open up App.js to start working on your app!! 22222</Text>
+				<Text>lol</Text>
+			</View>
+			<View className="border-black p-3">
+				<Text>Open up App.js to start working on your app!!</Text>
+				<Text>lol</Text>
+			</View>
+		</View>
+	);
+}
