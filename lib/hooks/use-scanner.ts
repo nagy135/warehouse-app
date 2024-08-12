a110c29e2db1d1961d1cd693893c35322ebbcc32
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-08-12 22:01:51 +0200

    feat: mocking of zebra scanner

--- lib/hooks/use-scanner.ts
+++ lib/hooks/use-scanner.ts
@@ -1,11 +1,21 @@
 import { useEffect, useState } from "react";
 import * as ExpoZebraScanner from "expo-zebra-scanner";
 
+const randomString = () => Math.random().toString(36).slice(2, 7);
+
 export default function useScanner() {
+
 	const [data, setData] = useState<string | null>(null);
 	const [label, setLabel] = useState<string | null>(null);
 	const [scanning, setScanning] = useState(false);
 	useEffect(() => {
+
+		if (process.env.MOCK_SCANNER === "true") {
+			setData("mocked_" + randomString());
+			setLabel("mocked_" + randomString());
+			setScanning(false);
+			return
+		}
 
 		const listener = ExpoZebraScanner.addListener(event => {
 
