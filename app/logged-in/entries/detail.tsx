8443b8fcd7c74646a66f83bed3e407691a9667e5
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-08-13 17:08:24 +0200

    style: detail page

--- app/logged-in/entries/detail.tsx
+++ app/logged-in/entries/detail.tsx
@@ -1,15 +1,32 @@
 import { useLocalSearchParams } from 'expo-router';
 import { View } from 'react-native';
 import { Text } from '~/components/ui/text';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '~/components/ui/card';
 import { Entry, type ToStringOrStringArray } from '~/lib/types';
 
 export default function DetailPage() {
 	const entry = useLocalSearchParams<ToStringOrStringArray<Entry>>();
 	return (
-		<View>
-			<Text>Detail Page</Text>
-			<Text>{entry.id}</Text>
-			<Text>{entry.name}</Text>
+		<View className="m-5">
+			<Card className='w-full'>
+				<CardHeader>
+					<CardTitle>{entry.name}</CardTitle>
+					<CardDescription>exit number #{entry.id}</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Text>Content</Text>
+				</CardContent>
+				<CardFooter>
+					<Text>Do stuff</Text>
+				</CardFooter>
+			</Card>
 		</View>
 	);
 }
--- app/logged-in/exits/detail.tsx
+++ app/logged-in/exits/detail.tsx
@@ -1,15 +1,32 @@
 import { useLocalSearchParams } from 'expo-router';
 import { View } from 'react-native';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '~/components/ui/card';
 import { Text } from '~/components/ui/text';
 import { Exit, type ToStringOrStringArray } from '~/lib/types';
 
 export default function DetailPage() {
 	const exit = useLocalSearchParams<ToStringOrStringArray<Exit>>();
 	return (
-		<View>
-			<Text>Detail Page</Text>
-			<Text>{exit.id}</Text>
-			<Text>{exit.name}</Text>
+		<View className="m-5">
+			<Card className='w-full'>
+				<CardHeader>
+					<CardTitle>{exit.name}</CardTitle>
+					<CardDescription>exit number #{exit.id}</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Text>Content</Text>
+				</CardContent>
+				<CardFooter>
+					<Text>Do stuff</Text>
+				</CardFooter>
+			</Card>
 		</View>
 	);
 }
