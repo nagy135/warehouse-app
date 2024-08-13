abee9f1f275f3189f0807f01acc262278fb5550e
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-08-13 12:54:48 +0200

    fix: theme unresponsive texts

--- components/zebra-scanner.tsx
+++ components/zebra-scanner.tsx
@@ -1,5 +1,6 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { Button } from "./ui/button";
+import { Text } from "./ui/text";
 import useScanner from "~/lib/hooks/use-scanner";
 
 export default function ZebraScanner() {
@@ -8,7 +9,11 @@ export default function ZebraScanner() {
 
 	return (
 		<View>
-			<Button onPress={() => startScan()}><Text>Start Scan</Text></Button>
+			<Button
+				onPress={() => startScan()}
+				size="lg">
+				<Text className=" text-2xl font-bold">SCAN!</Text>
+			</Button>
 			<Text>{scanning ? 'SCANNING' : 'not scanning'}</Text>
 			<Text>{`data: ${data}`}</Text>
 			<Text>{`label: ${label}`}</Text>
