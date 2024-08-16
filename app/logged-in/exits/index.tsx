75e4a0c03adc1c104a09fc0a63cb66b8dde2fcdd
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-08-16 13:41:08 +0200

    fix: forgotten update of exits after hook change

--- app/logged-in/exits/index.tsx
+++ app/logged-in/exits/index.tsx
@@ -12,10 +12,12 @@ export default function ExitsPage() {
 	const {
 		data: exits,
 		isWaiting,
+		isLoading,
+		error,
 		refreshing,
 		onRefresh,
 	} = useGetRecords<Exit>('exits', searchValue);
-	if (!Array.isArray(exits.data) && !exits.isLoading) return <Text>error</Text>;
+	if (error) return <Text>error</Text>;
 
 	return (
 		<>
@@ -34,7 +36,7 @@ export default function ExitsPage() {
 					}
 				>
 					{
-						exits.data?.map((exit: Exit, i: number) => (
+						exits?.map((exit: Exit, i: number) => (
 							<View key={`exit-card-${i}`} className="my-1">
 								<ExitCard exit={exit} />
 							</View>
@@ -42,7 +44,7 @@ export default function ExitsPage() {
 					}
 				</ScrollView>
 			</View>
-			{isWaiting &&
+			{(isWaiting || isLoading) &&
 				<View className="absolute top-0 left-0 right-0 bottom-0 items-center justify-center">
 					<ActivityIndicator size={60} color="#666666" />
 				</View>}
