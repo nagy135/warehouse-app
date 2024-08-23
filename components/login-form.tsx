992bdc101447100e4ab742feaf2d7ec0366cbd0e
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-08-23 21:05:46 +0200

    feat: bypass fake login loader with env var

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,113 +1,129 @@
-import * as React from 'react';
-import { Alert, View } from 'react-native';
-import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
-import { Button } from '~/components/ui/button';
-import { useSession } from '~/ctx';
-import { router } from 'expo-router';
+import * as React from "react";
+import { Alert, View } from "react-native";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { Button } from "~/components/ui/button";
+import { useSession } from "~/ctx";
+import { router } from "expo-router";
 import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-} from '~/components/ui/card';
-import { Progress } from '~/components/ui/progress';
-import { Text } from '~/components/ui/text';
-import { Input } from '~/components/ui/input';
-import { useRef } from 'react';
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "~/components/ui/card";
+import { Progress } from "~/components/ui/progress";
+import { Text } from "~/components/ui/text";
+import { Input } from "~/components/ui/input";
+import { useRef } from "react";
+import { isEnvVar } from "~/lib/utils";
 
 const AVATAR_URI =
-	'https://i.pinimg.com/736x/3f/94/70/3f9470b34a8e3f526dbdb022f9f19cf7.jpg';
-
+  "https://i.pinimg.com/736x/3f/94/70/3f9470b34a8e3f526dbdb022f9f19cf7.jpg";
 
 export default function LoginForm() {
-	const [showPassword, setShowPassword] = React.useState(false);
-	const [email, setEmail] = React.useState('user@user.com');
-	const [password, setPassword] = React.useState('user');
-	const [progress, setProgress] = React.useState(0);
-	const [loggingIn, setLoggingIn] = React.useState(false);
-	const timerRef = useRef<NodeJS.Timeout | null>(null);
-	const { signIn } = useSession();
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [email, setEmail] = React.useState("user@user.com");
+  const [password, setPassword] = React.useState("user");
+  const [progress, setProgress] = React.useState(0);
+  const [loggingIn, setLoggingIn] = React.useState(false);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+  const { signIn } = useSession();
 
+  React.useEffect(() => {
+    if (!loggingIn) return;
+    if (progress >= 100) {
+      signIn(email, password)
+        .then(() => {
+          router.replace("/logged-in");
+        })
+        .catch(() => {
+          Alert.alert("Login error", "Could not log in.", [
+            {
+              text: "OK",
+              onPress: () => {
+                setProgress(0);
+                setLoggingIn(false);
+                clearTimeout(timerRef.current!);
+              },
+              style: "default",
+            },
+          ]);
+        });
+    } else {
+      if (isEnvVar("FAKE_LOGIN_LOADER", false)) {
+        setProgress(100);
+        return;
+      }
+      timerRef.current = setTimeout(() => {
+        setProgress(progress + 10);
+      }, (Math.random() * 1000) / 2);
+    }
+  }, [progress, setProgress, loggingIn]);
 
-	React.useEffect(() => {
-		if (!loggingIn) return;
-		if (progress >= 100) {
-			signIn(email, password).then(() => {
-				router.replace('/logged-in');
-			}).catch(() => {
-				Alert.alert('Login error', 'Could not log in.', [
-					{ text: 'OK', onPress: () => { setProgress(0); setLoggingIn(false); clearTimeout(timerRef.current!); }, style: 'default' },
-				]);
-			});
+  const handleLogInPress = React.useCallback(() => {
+    setLoggingIn(true);
+  }, [email, password]);
+  return (
+    <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
+      <Card className="w-full max-w-sm p-6 rounded-2xl">
+        <CardHeader className="items-center">
+          <Avatar alt="Rick Sanchez's Avatar" className="w-24 h-24">
+            <AvatarImage source={{ uri: AVATAR_URI }} />
+            <AvatarFallback>
+              <Text>RS</Text>
+            </AvatarFallback>
+          </Avatar>
+          <View className="p-3" />
+        </CardHeader>
+        <CardContent>
+          <View className="flex gap-3">
+            <Input
+              value={email}
+              onChangeText={setEmail}
+              aria-labelledby="inputLabel"
+              aria-errormessage="inputError"
+            />
+            <View className="flex-row justify-end gap-3">
+              <Input
+                className="flex-1"
+                secureTextEntry={showPassword}
+                value={password}
+                onChangeText={setPassword}
+                aria-labelledby="inputLabel"
+                aria-errormessage="inputError"
+              />
+              <Button
+                variant="outline"
+                className="shadow shadow-foreground/5"
+                onPress={
+                  showPassword
+                    ? () => setShowPassword(false)
+                    : () => setShowPassword(true)
+                }
+              >
+                <Text>{!showPassword ? "Hide" : "Show"}</Text>
+              </Button>
+            </View>
+          </View>
+        </CardContent>
+        <CardFooter className="flex-col gap-3 pb-0">
+          {progress > 0 ? (
+            <Progress
+              value={progress}
+              className="h-2"
+              indicatorClassName="bg-sky-600"
+            />
+          ) : null}
+          <View />
 
-		} else {
-			timerRef.current = setTimeout(() => {
-				setProgress(progress + 10);
-			}, Math.random() * 1000 / 2);
-		}
-
-	}, [progress, setProgress, loggingIn])
-
-
-	const handleLogInPress = React.useCallback(() => {
-		setLoggingIn(true);
-	}, [email, password]);
-	return (
-		<View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
-			<Card className='w-full max-w-sm p-6 rounded-2xl'>
-				<CardHeader className='items-center'>
-					<Avatar alt="Rick Sanchez's Avatar" className='w-24 h-24'>
-						<AvatarImage source={{ uri: AVATAR_URI }} />
-						<AvatarFallback>
-							<Text>RS</Text>
-						</AvatarFallback>
-					</Avatar>
-					<View className='p-3' />
-				</CardHeader>
-				<CardContent>
-					<View className="flex gap-3">
-						<Input
-							value={email}
-							onChangeText={setEmail}
-							aria-labelledby='inputLabel'
-							aria-errormessage='inputError'
-						/>
-						<View className="flex-row justify-end gap-3">
-							<Input
-								className="flex-1"
-								secureTextEntry={showPassword}
-								value={password}
-								onChangeText={setPassword}
-								aria-labelledby='inputLabel'
-								aria-errormessage='inputError'
-							/>
-							<Button
-								variant='outline'
-								className='shadow shadow-foreground/5'
-								onPress={showPassword ? () => setShowPassword(false) : () => setShowPassword(true)}
-							>
-								<Text>{!showPassword ? "Hide" : "Show"}</Text>
-							</Button>
-						</View>
-					</View>
-				</CardContent>
-				<CardFooter className='flex-col gap-3 pb-0'>
-					{progress > 0 ? <Progress
-						value={progress}
-						className='h-2'
-						indicatorClassName='bg-sky-600'
-					/> : null}
-					<View />
-
-					<Button
-						variant='outline'
-						className='shadow shadow-foreground/5'
-						onPress={handleLogInPress}
-					>
-						<Text>Log in</Text>
-					</Button>
-				</CardFooter>
-			</Card>
-		</View>
-	);
+          <Button
+            variant="outline"
+            className="shadow shadow-foreground/5"
+            onPress={handleLogInPress}
+          >
+            <Text>Log in</Text>
+          </Button>
+        </CardFooter>
+      </Card>
+    </View>
+  );
 }
--- lib/types.ts
+++ lib/types.ts
@@ -1,59 +1,57 @@
 export type Entry = {
-	id: number;
-	name: string;
-	sku: string;
-	createdAt: string;
-	createdById: string;
+  id: number;
+  name: string;
+  sku: string;
+  createdAt: string;
+  createdById: string;
 };
 
 export type Exit = {
-	id: number;
-	name: string;
-	sku: string;
-	createdAt: string;
-	createdById: string;
-	productStorages?: ProductStorage[]
+  id: number;
+  name: string;
+  sku: string;
+  createdAt: string;
+  createdById: string;
+  productStorages?: ProductStorage[];
 };
 
 export type ProductStorage = {
-	productSkuVariant: ProductSkuVariant;
-	storage: Storage;
+  productSkuVariant: ProductSkuVariant;
+  storage: Storage;
 };
 
 export type ProductSkuVariant = {
-	id: number;
-	name: string;
-	sku: string;
-	productChangeVariant: ProductChangeVariant;
-	productDeliveryVariant: ProductDeliveryVariant;
+  id: number;
+  name: string;
+  sku: string;
+  productCV: ProductCV;
+  productDV: ProductDV;
 };
 
-export type ProductChangeVariant = {
-	change: string;
-	value: string;
-	product?: Product;
+export type ProductCV = {
+  change: string;
+  value: string;
+  product?: Product;
 };
 
-export type ProductDeliveryVariant = {
-	height: number;
-	width: number;
-	depth: number;
-	weight: number;
-	product?: Product;
+export type ProductDV = {
+  height: number;
+  width: number;
+  depth: number;
+  weight: number;
+  product?: Product;
 };
 
 export type Product = {
-	id: number;
-	name: string;
-	sku: string;
-	createdAt: string;
-	createdById: string;
+  id: number;
+  name: string;
+  sku: string;
+  createdAt: string;
+  createdById: string;
 };
-
-
 
 // cast every value to string or string[] that isnt that already
 // this is needed because expo router stringifies every value
 export type ToStringOrStringArray<T> = {
-	[K in keyof T]: T[K] extends string | string[] ? T[K] : string;
+  [K in keyof T]: T[K] extends string | string[] ? T[K] : string;
 };
